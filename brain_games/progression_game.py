57ab8b6c3556e080cb8b22e84215d8fd7949185b
Sergey Balashov <balashevski@gmail.com>
2024-03-10 13:47:13 +0400

    corrected linter mistakes of step 8

--- brain_games/progression_game.py
+++ brain_games/progression_game.py
@@ -1,6 +1,7 @@
 import brain_games.common_game_logic as gl
 from brain_games.even_game import get_random_number
 
+
 def format_prog_list_into_str(prog_list, index_to_omit):
     i = 0
     prog_len = len(prog_list)
@@ -8,16 +9,17 @@ def format_prog_list_into_str(prog_list, index_to_omit):
     for i in range(prog_len):
         if i != index_to_omit:
             prog_str += str(prog_list[i])
-        else: 
+        else:
             prog_str += ".."
         if i != prog_len - 1:
             prog_str += " "
     return prog_str
 
+
 def get_quest_and_answ_prog():
     prog_len = get_random_number(5, 10)
     prog_step = get_random_number(-50, 50)
-    prog_start = get_random_number(0,20)
+    prog_start = get_random_number(0, 20)
     prog_list = []
     for i in range(prog_len):
         prog_list.append(prog_start + (i * prog_step))
